703c30e83011da4f56b0c0adae49cb0142e17eb5
森重ゆう <morishige_yu@morishigeyuu-no-MacBook-Air.local>
2017-12-14 10:47:13 +0900

    ユーザ登録部分

--- Gemfile
+++ Gemfile
@@ -56,3 +56,9 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 # bootstrap4
 gem 'bootstrap', '~> 4.0.0.beta2.1'
 gem 'jquery-rails'
+
+# login
+gem 'devise'
+
+#ja
+gem 'rails-i18n'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'balances#index'
+  #root to: 'tops#index'
+  devise_for :users
+  devise_scope :user do
+    root :to => "devise/sessions#new"
+  end
+  resources :balances
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
